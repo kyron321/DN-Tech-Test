227d6e5a3a88ac024a0138011136392c78d52510
Kyron Smith <kyronsmith@Kyrons-MacBook-Pro.local>
2023-05-27 10:02:28 +0100

    feat: card validation and error handling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const nameInfo = document.getElementById("name");
   const emailInfo = document.getElementById("email");
+  const cardInfo = document.getElementById("card");
 
   nameInfo.addEventListener("input", () => {
     const name = nameInfo.value.trim();
@@ -22,6 +23,18 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  cardInfo.addEventListener("input", () => {
+    const card = cardInfo.value.trim();
+    const numericTest = /^[0-9]+$/;
+    if (!numericTest.test(card)) {
+      showError(cardInfo, "Please enter a valid credit card number");
+    } else if (!validateCreditCard(card)) {
+      showError(cardInfo, "Invalid credit card number");
+    } else {
+      hideError(cardInfo);
+    }
+  });
+
   function showError(input, message) {
     const errorDiv = input.nextElementSibling;
     errorDiv.textContent = message;
@@ -34,5 +47,26 @@ document.addEventListener("DOMContentLoaded", () => {
     errorDiv.textContent = "";
     errorDiv.style.display = "none";
     input.style.backgroundColor = "#e2f2cc";
+  }
+
+  function validateCreditCard(cardNumber) {
+    let digits = cardNumber.split("").map(Number);
+    let oddSum = 0;
+    let evenSum = 0;
+    let len = digits.length;
+
+    for (let i = 0; i < len; i++) {
+      if ((len - i) % 2 === 0) {
+        let val = 2 * digits[i];
+        if (val > 9) {
+          val -= 9;
+        }
+        evenSum += val;
+      } else {
+        oddSum += digits[i];
+      }
+    }
+
+    return (oddSum + evenSum) % 10 === 0;
   }
 });
